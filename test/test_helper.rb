e6f40c55cf88a40db75d2d9c17aa7425fb691015
Ben Mabey <ben@benmabey.com>
2011-12-19 22:25:46 -0700

    how I would use database cleaner...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,13 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   self.use_transactional_fixtures = false
-  db_cleaner = DatabaseCleaner[:active_record, :connection => 'test']
-  db_cleaner.strategy = :transaction
+  DatabaseCleaner.strategy = :transaction
 
   setup do
-    db_cleaner.start
+    DatabaseCleaner.start
   end
 
   teardown do
-    db_cleaner.clean
+    DatabaseCleaner.clean
   end
 end
